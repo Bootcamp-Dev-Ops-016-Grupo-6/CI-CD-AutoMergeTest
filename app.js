53baf6abeb1644eb0a6eae203ddf0d9799cfbfb0
José Albornoz <josealbornozsalazar@gmail.com>
2025-07-14 22:28:44 -0400

    probando error

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ function getWelcomeMessage() {
   return '¡Hola mundo desde App desplegada automáticamente con Github Actions!';
 }
 if (typeof document !== 'undefined') {
-  document.getElementById('message').textContent = getWelcomeMessage();
+  document.getElementById('message').textContent = 
 }
 module.exports = getWelcomeMessage;
